75595ddd2e3cf10ef32c0925f1e1d99289649e00
ciaranRoche <ciaranroche@hotmail.co.uk>
2019-02-26 20:07:06 +0000

    inital func handler

--- src/main/java/R3x.java
+++ src/main/java/R3x.java
@@ -1,5 +1,41 @@
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class R3x {
-    public static void main(String[] args){
-        System.out.println("Hello R3X");
+
+    // Handle User Function
+    public void execute(Runnable r3x) {
+        try {
+            HTTPStream(r3x);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void HTTPStream(Runnable r3x) throws IOException {
+        r3x.run();
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null);
+        server.start();
+    }
+
+    static class MyHandler implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange httpExchange) throws IOException {
+            String response = "This is the response";
+            httpExchange.sendResponseHeaders(200, response.length());
+            OutputStream os = httpExchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
     }
 }
+
+
---
+++ src/main/java/R3xHelloWorld.java
@@ -0,0 +1,15 @@
+import com.google.gson.JsonObject;
+
+public class R3xHelloWorld {
+    public static void main(String[] args){
+        R3x r3x = new R3x();
+        r3x.execute(R3xHelloWorld::r3xFunc);
+    }
+
+    private static JsonObject r3xFunc(){
+        JsonObject res = new JsonObject();
+        res.addProperty("message", "Hello r3x");
+        System.out.println(res);
+        return res;
+    }
+}
