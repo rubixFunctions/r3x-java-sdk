710f77fba27d4e64102af2b653d40f10bc2120cd
ciaranRoche <ciaranroche@hotmail.co.uk>
2019-03-08 12:50:56 +0000

    add error handling

--- src/main/java/handlers/RubixJsonHandler.java
+++ src/main/java/handlers/RubixJsonHandler.java
@@ -12,4 +12,16 @@ class RubixJsonHandler {
         JsonObject json = (JsonObject) parser.parse(s);
         return json;
     }
+
+    public boolean isValid(String json){
+        try {
+            new JsonParser().parse(json);
+            return true;
+        } catch (JsonParseException e) {
+            System.out.println("bad json: " + json);
+            return false;
+        } catch( NullPointerException e) {
+            return false;
+        }
+    }
 }
--- src/main/java/handlers/RubixResHandler.java
+++ src/main/java/handlers/RubixResHandler.java
@@ -1,5 +1,7 @@
 package handlers;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -15,22 +17,49 @@ public class RubixResHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
+        String method = httpExchange.getRequestMethod();
+        if ("post".equalsIgnoreCase(method)) {
+            handleRequest(httpExchange);
+        } else {
+            handleError(httpExchange,errorJson("Only POST supported") );
+        }
+    }
+
+    private void handleRequest(HttpExchange httpExchange) throws IOException{
         InputStream input = httpExchange.getRequestBody();
 
         StringBuilder sb = new StringBuilder();
         new BufferedReader(new InputStreamReader(input))
                 .lines()
-                .forEach((String s) -> sb.append(s));
+                .forEach(sb::append);
 
         RubixJsonHandler jsonHandler = new RubixJsonHandler();
-        JsonObject json = jsonHandler.processString(sb.toString());
+        if (jsonHandler.isValid(sb.toString())) {
+            JsonObject json = jsonHandler.processString(sb.toString());
 
-        String response = String.valueOf(this.handler.execute(json));
+            String response = String.valueOf(this.handler.execute(json));
+            httpExchange.getResponseHeaders().set("Content-Type", "application/json");
+            httpExchange.sendResponseHeaders(200, response.length());
+
+            OutputStream os = httpExchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        } else {
+            handleError(httpExchange,errorJson("JSON Format Error") );
+        }
+    }
+
+    private void handleError(HttpExchange httpExchange, String response) throws IOException {
         httpExchange.getResponseHeaders().set("Content-Type", "application/json");
-        httpExchange.sendResponseHeaders(200, response.length());
+        httpExchange.sendResponseHeaders(500, response.length());
 
         OutputStream os = httpExchange.getResponseBody();
         os.write(response.getBytes());
         os.close();
     }
+
+    private String errorJson(String message) {
+        return String.format("{\"ERROR\":\"%s\"}", message);
+    }
+
 }
